c013241964ed98d695c1854c69186d8ba501b0ed
khalidengineer <61066087+khalidengineer@users.noreply.github.com>
2026-09-13 09:41:26 +0530

    l

--- app/layout.tsx
+++ app/layout.tsx
@@ -186,13 +186,7 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
         />
-        <script type="text/javascript">
-          (function(c,l,a,r,i,t,y){
-            c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
-          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-           })(window, document, "clarity", "script", "yhhe8erzdc");
-        </script>
+        
       </head>
       <body
         suppressHydrationWarning
